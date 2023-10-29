415aa6318f01e11bf940422343f0fc8c966a0809
TeodorMichalBaranski <126625089+TeodorMichalBaranski@users.noreply.github.com>
2023-10-29 14:55:40 +0100

    changes. clearing the code. moving functionality to shoppingListCard.html

--- src/js/shoppingList.js
+++ src/js/shoppingList.js
@@ -12,10 +12,126 @@ shoppingListEl: document.querySelector('.shopping__cards');
 notificationContainerEl: document.querySelector('.shopping__storage');
 shoppingHeadingEl: document.querySelector('.shopping__heading');
 
-const fetchBooksButton = document.getElementById('fetchBooks');
-fetchBooksButton.addEventListener('click', displayBooks);
+//------------------
+//pobierz i zapisz ID wszystkich ksiazek do local storage
+
+function saveTopBooks() {
+  getTopBooks()
+    .then(response => {
+      console.log(response);
+      if (!response || response.length === 0) {
+        console.error('Nie znaleziono listy książek.');
+        return;
+      }
+
+      const allBookIds = [];
+
+      response.forEach(list => {
+        if (list.books && list.books.length > 0) {
+          const bookIds = list.books.map(book => book._id);
+          allBookIds.push(...bookIds);
+        }
+      });
+
+      console.log('ID wszystkich książek z listy:', allBookIds);
+      storageMethods.save('selected-books', allBookIds);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+document.getElementById('save-books-button').addEventListener('click', saveTopBooks);
+
+// Wykorzystanie innerHTML w celu tworzenia elementów HTML z kodem źródłowym może być podatne na ataki XSS (Cross-Site Scripting), jeśli dane pochodzą od użytkowników lub są niezaufane.
+//  Możesz rozważyć użycie bardziej bezpiecznych metod, takich jak document.createElement, aby utworzyć elementy HTML.
+// Przed użyciem wartości z obiektu book, upewnij się, że dane są bezpieczne i nie zawierają niebezpiecznych skryptów.
+
+//Load Id from locale storage, fetch books props via API. Render markup
+document.getElementById('load-books-and-render').addEventListener('click', loadAndRenderBooks);
+
+async function loadAndRenderBooks() {
+  try {
+    // Retrieve stored book IDs from local storage
+    const storedBookIds = storageMethods.load('selected-books');
+    if (!storedBookIds || storedBookIds.length === 0) {
+      console.error('No book IDs found in local storage.');
+      return;
+    }
+
+    // Fetch details for each book using the IDs
+    const booksDetails = await Promise.all(storedBookIds.map(id => getBooksId(id)));
+
+    // Log the fetched details for debugging
+    console.log('Fetched book details:', booksDetails);
+
+    // Select the ul where you want to render the books
+    const container = document.querySelector('.shopping__cards');
+
+    const markup = booksDetails
+      .map(
+        ({
+          _id,
+          book_image,
+          author,
+          book_image_width,
+          book_image_height,
+          title,
+          list_name,
+          description,
+          buy_links: [amazon, apple, , , bookshop],
+        }) => {
+          return `<li class="shopping__card" data-id="${_id}">
+      <div class="shopping__block">
+        <div>
+          <div class="shopping__thumb">
+            <img src="${book_image}" alt="${list_name}" class="shopping__book-img" width="${book_image_width}" height="${book_image_height}"/>
+          </div>
+          <p class="shopping__book-author">${author}</p>
+        </div>
+        <div class="shopping__wrap">
+          <h2 class="shopping__title">${title}</h2>
+          <p class="shopping__category">${list_name}</p>
+          <p class="shopping__book-description--tablet">${description}</p>
+          <ul class="shopping__shops">
+            <li class="shopping__shop">
+              <a href="${amazon.url}" class="shopping__shop-link" target="_blank" crossorigin="anonymous"  rel="noopener noreferrer" aria-label="Amazon-book site">
+                <img srcset="${amazonImage1} 1x, ${amazonImage2} 2x" src="${amazonImage1}" alt="${amazon.name}" class="shopping__shop-img" width="48" height="15"/>
+              </a>
+            </li>
+            <li class="shopping__shop">
+              <a href="${apple.url}" class="shopping__shop-link" target="_blank" crossorigin="anonymous"  rel="noopener noreferrer" aria-label="Apple-book site">
+                <img srcset="${appleImage1} 1x, ${appleImage2} 2x" src="${appleImage1}" alt="${apple.name}" class="shopping__shop-img" width="28" height="27"/>
+              </a>
+            </li>
+            <li class="shopping__shop">
+              <a href="${bookshop.url}" class="shopping__shop-link" target="_blank" crossorigin="anonymous"  rel="noopener noreferrer" aria-label="Book-shop site">
+                <img srcset="${bookshopImage1} 1x, ${bookshopImage2} 2x" src="${bookshopImage1}" alt="${bookshop.name}" class="shopping__shop-img" width="32" height="30"/>
+              </a>
+            </li>
+          </ul>
+          
+          </div>
+      </div>
+      <button type="button" class="shopping__btn" aria-label="Delete the book from shopping list">
+        <svg class="shopping__btn-icon" width="18" height="18">
+          <use href="#icon-dump"></use>
+        </svg>
+      </button>
+      <p class="shopping__book-description">${description}</p>
+    </li>`;
+        },
+      )
+      .join('');
+
+    container.innerHTML = markup;
+  } catch (error) {
+    console.error('Error while loading and rendering books:', error);
+  }
+}
+
 //---------------------
-// importowanie ksiazek toopbooks aaa
+// importowanie ksiazek toopbooks
 // async function fetchAndLogTopBooks() {
 //   try {
 //     const books = await getTopBooks();
@@ -34,45 +150,47 @@ fetchBooksButton.addEventListener('click', displayBooks);
 //---------------------
 
 //---------------------
+// const fetchBooksButton = document.getElementById('fetchBooks');
+// fetchBooksButton.addEventListener('click', displayBooks);
 // pobiez top books i wyswietl prosty markup
-function displayBooks() {
-  const booksContainer = document.getElementById('books-container');
+// function displayBooks() {
+//   const booksContainer = document.getElementById('books-container');
 
-  getTopBooks()
-    .then(response => {
-      console.log(response);
-      const list = response[0];
-      if (!list || !list.books) {
-        console.error('Nie znaleziono książek.');
-        return;
-      }
-      const books = list.books;
-      books.slice(0, 5).forEach(book => {
-        const bookElement = document.createElement('div');
-        bookElement.innerHTML = `
-            <h3>${book.title}</h3>
-            <img src="${book.book_image}" alt="${book.title}" width="${
-          book.book_image_width
-        }" height="${book.book_image_height}">
-            <p>Author: ${book.author}</p>
-            <p>${book.description}</p>
-            <div class="buy-links">
-              ${book.buy_links
-                .map(
-                  link => `
-                <a href="${link.url}" target="_blank">Buy on ${link.name}</a>
-              `,
-                )
-                .join('')}
-            </div>
-          `;
-        booksContainer.appendChild(bookElement);
-      });
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
+//   getTopBooks()
+//     .then(response => {
+//       console.log(response);
+//       const list = response[0];
+//       if (!list || !list.books) {
+//         console.error('Nie znaleziono książek.');
+//         return;
+//       }
+//       const books = list.books;
+//       books.slice(0, 5).forEach(book => {
+//         const bookElement = document.createElement('div');
+//         bookElement.innerHTML = `
+//             <h3>${book.title}</h3>
+//             <img src="${book.book_image}" alt="${book.title}" width="${
+//           book.book_image_width
+//         }" height="${book.book_image_height}">
+//             <p>Author: ${book.author}</p>
+//             <p>${book.description}</p>
+//             <div class="buy-links">
+//               ${book.buy_links
+//                 .map(
+//                   link => `
+//                 <a href="${link.url}" target="_blank">Buy on ${link.name}</a>
+//               `,
+//                 )
+//                 .join('')}
+//             </div>
+//           `;
+//         booksContainer.appendChild(bookElement);
+//       });
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// }
 
 //--------------------
 //Pobierz 5 top books i zapisz w local storage
@@ -113,40 +231,6 @@ function displayBooks() {
 //       console.log(error);
 //     });
 // }
-//------------------
-//pobierz i zapisz ID wszystkich ksiazek do local storage
-
-function saveTopBooks() {
-  getTopBooks()
-    .then(response => {
-      console.log(response);
-      if (!response || response.length === 0) {
-        console.error('Nie znaleziono listy książek.');
-        return;
-      }
-
-      const allBookIds = [];
-
-      response.forEach(list => {
-        if (list.books && list.books.length > 0) {
-          const bookIds = list.books.map(book => book._id);
-          allBookIds.push(...bookIds);
-        }
-      });
-
-      console.log('ID wszystkich książek z listy:', allBookIds);
-      storageMethods.save('selected-books', allBookIds);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
-
-document.getElementById('save-books-button').addEventListener('click', saveTopBooks);
-
-// Wykorzystanie innerHTML w celu tworzenia elementów HTML z kodem źródłowym może być podatne na ataki XSS (Cross-Site Scripting), jeśli dane pochodzą od użytkowników lub są niezaufane.
-//  Możesz rozważyć użycie bardziej bezpiecznych metod, takich jak document.createElement, aby utworzyć elementy HTML.
-// Przed użyciem wartości z obiektu book, upewnij się, że dane są bezpieczne i nie zawierają niebezpiecznych skryptów.
 
 // zaladuj id z local storage i pobierz dane o tych ksiazkach z api.
 // async function loadAndRenderBooks() {
@@ -233,85 +317,3 @@ document.getElementById('save-books-button').addEventListener('click', saveTopBo
 //     console.error('Error while loading and rendering books:', error);
 //   }
 // }
-
-document.getElementById('load-books-and-render').addEventListener('click', loadAndRenderBooks);
-
-async function loadAndRenderBooks() {
-  try {
-    // Retrieve stored book IDs from local storage
-    const storedBookIds = storageMethods.load('selected-books');
-    if (!storedBookIds || storedBookIds.length === 0) {
-      console.error('No book IDs found in local storage.');
-      return;
-    }
-
-    // Fetch details for each book using the IDs
-    const booksDetails = await Promise.all(storedBookIds.map(id => getBooksId(id)));
-
-    // Log the fetched details for debugging
-    console.log('Fetched book details:', booksDetails);
-
-    // Select the ul where you want to render the books
-    const container = document.querySelector('.shopping__cards');
-
-    const markup = booksDetails
-      .map(
-        ({
-          _id,
-          book_image,
-          author,
-          book_image_width,
-          book_image_height,
-          title,
-          list_name,
-          description,
-          buy_links: [amazon, apple, , , bookshop],
-        }) => {
-          return `<li class="shopping__card" data-id="${_id}">
-      <div class="shopping__block">
-        <div>
-          <div class="shopping__thumb">
-            <img src="${book_image}" alt="${list_name}" class="shopping__book-img" width="${book_image_width}" height="${book_image_height}"/>
-          </div>
-          <p class="shopping__book-author">${author}</p>
-        </div>
-        <div class="shopping__wrap">
-          <h2 class="shopping__title">${title}</h2>
-          <p class="shopping__category">${list_name}</p>
-          <p class="shopping__book-description--tablet">${description}</p>
-          <ul class="shopping__shops">
-            <li class="shopping__shop">
-              <a href="${amazon.url}" class="shopping__shop-link" target="_blank" crossorigin="anonymous"  rel="noopener noreferrer" aria-label="Amazon-book site">
-                <img srcset="${amazonImage1} 1x, ${amazonImage2} 2x" src="${amazonImage1}" alt="${amazon.name}" class="shopping__shop-img" width="48" height="15"/>
-              </a>
-            </li>
-            <li class="shopping__shop">
-              <a href="${apple.url}" class="shopping__shop-link" target="_blank" crossorigin="anonymous"  rel="noopener noreferrer" aria-label="Apple-book site">
-                <img srcset="${appleImage1} 1x, ${appleImage2} 2x" src="${appleImage1}" alt="${apple.name}" class="shopping__shop-img" width="28" height="27"/>
-              </a>
-            </li>
-            <li class="shopping__shop">
-              <a href="${bookshop.url}" class="shopping__shop-link" target="_blank" crossorigin="anonymous"  rel="noopener noreferrer" aria-label="Book-shop site">
-                <img srcset="${bookshopImage1} 1x, ${bookshopImage2} 2x" src="${bookshopImage1}" alt="${bookshop.name}" class="shopping__shop-img" width="32" height="30"/>
-              </a>
-            </li>
-          </ul>
-          
-          </div>
-      </div>
-      <button type="button" class="shopping__btn" aria-label="Delete the book from shopping list">
-        <svg class="shopping__btn-icon" width="18" height="18">
-          <use href="#icon-dump"></use>
-        </svg>
-      </button>
-      <p class="shopping__book-description">${description}</p>
-    </li>`;
-        },
-      )
-      .join('');
-
-    container.innerHTML = markup;
-  } catch (error) {
-    console.error('Error while loading and rendering books:', error);
-  }
-}
