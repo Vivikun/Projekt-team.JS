8ffca9774fe017c749bd3865dce12efb3a9d6d48
Xrolllex <karolkaczanowski@wp.pl>
2023-10-29 11:26:46 +0100

    Commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './sass/main.scss';
 import './js/book-api.js';
 import './js/home-categories.js';
-// import './js/mobile-menu.js';
+import './js/mobile-menu.js';
 import './js/shoppingList.js';
 import './UKRAINE-slider.js';
 import './js/home-books.js';
--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,54 +1,25 @@
-// const mobileMenu = document.querySelector('.js-menu-container');
-// const openMenuBtn = document.querySelector('.js-open-menu');
-// const closeMenuBtn = document.querySelector('.js-close-menu');
+const mobileMenu = document.querySelector(".js-menu-container");
+    const openMenuBtn = document.querySelector(".js-open-menu");
 
-// const toggleMenu = () => {
-//   const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-//   openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-//   mobileMenu.classList.toggle('is-open');
+    const toggleMenu = () => {
+      const isMenuOpen =
+        openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+      openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+      mobileMenu.classList.toggle("is-open");
+      openMenuBtn.classList.toggle("opened")
 
-//   const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-//   bodyScrollLock[scrollLockMethod](document.body);
-// };
+      const scrollLockMethod = !isMenuOpen
+        ? "disableBodyScroll"
+        : "enableBodyScroll";
+      bodyScrollLock[scrollLockMethod](document.body);
+    };
 
-// openMenuBtn.addEventListener('click', toggleMenu);
-// closeMenuBtn.addEventListener('click', toggleMenu);
+    openMenuBtn.addEventListener("click", toggleMenu);
 
-// window
-//   .matchMedia('(min-width: 768px) and (orientation: portrait)')
-//   .addEventListener('change', element => {
-//     if (!element.matches) {
-//       mobileMenu.classList.remove('is-open');
-//       openMenuBtn.setAttribute('aria-expanded', false);
-//       bodyScrollLock.enableBodyScroll(document.body);
-//     }
-//   });
-const mobileMenu = document.querySelector('.js-menu-container');
-const openMenuBtn = document.querySelector('.js-open-menu');
-// const closeMenuBtn = document.querySelector('.js-close-menu');
-const mediaQuery = window.matchMedia('(min-width: 768px) and (orientation: portrait)');
-
-const toggleMenu = () => {
-  const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-  openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-  mobileMenu.classList.toggle('is-open');
-  const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-  bodyScrollLock[scrollLockMethod](document.body);
-};
-
-const closeMenu = () => {
-  mobileMenu.classList.remove('is-open');
-  openMenuBtn.setAttribute('aria-expanded', false);
-  bodyScrollLock.enableBodyScroll(document.body);
-};
-
-openMenuBtn.addEventListener('click', toggleMenu);
-// closeMenuBtn.addEventListener('click', closeMenu);
-
-mediaQuery.addEventListener('change', element => {
-  if (!element.matches) {
-    closeMenu();
-  }
-});
-
-//  brakuje jednego ważnego elementu - deklaracji i importu biblioteki bodyScrollLock.
+    window.matchMedia("(min-width: 768px) and (orientation: portrait)").addEventListener("change", (element) => {
+      if (!element.matches) {
+        mobileMenu.classList.remove("is-open");
+        openMenuBtn.setAttribute("aria-expanded", false);
+        bodyScrollLock.enableBodyScroll(document.body);
+      }
+    });
